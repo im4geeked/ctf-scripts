0952045b20d862474c0294bffc0d79af999a5c26
Zack <im4geeked@proton.me>
2026-07-30 01:52:56 +0500

    Added gaming directory

---
+++ gaming/sklauncher.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash 
+
+JAR_FILE=$(ls /home/$USER/Downloads/SK*.jar | head -n 1) # change this if you want 
+
+
+if [[ -f "$JAR_FILE" ]]; then
+	echo "[+] JAR file found..."
+	# DRI_PRIME=1 is just an environment variable to force a command to force my AMD GPU because it doesn't get used by default :(
+	DRI_PRIME=1 java -jar "$JAR_FILE" 
+	echo "[-] Not found. "
+	echo "[-] Failed"
+	exit 1
+fi
+
